b484f962e646bae3b0c5c8d1a10e242dcc8fb7e8
chimekinglsey <onlinekingsley@gmail.com>
2023-10-18 00:13:58 +0100

    update on stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 def log_stat(nginx_collection):
     """provides some stats about nginx logs stored in MongoDB"""
-    print(f'{nginx_collection.count_documents()} logs')
+    print(f'{nginx_collection.count_documents({})} logs')
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
