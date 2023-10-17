9d1eba5203eaebeae1935746f1e24319f3c31c09
chimekinglsey <onlinekingsley@gmail.com>
2023-10-17 22:44:43 +0100

    insert a new document to collection and return _id

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,7 +5,7 @@
 def insert_school(mongo_collection, **kwargs):
     """ insert a new document to collection and return _id"""
     contents = {}
-    for k,v in kwargs.items():
+    for k, v in kwargs.items():
         contents.update({k: v})
     insert_value = mongo_collection.insert_one(contents)
     return insert_value.inserted_id
