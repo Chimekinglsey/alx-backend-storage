caabe4c96e8b7ba0c6ee95fb2052b99dab4afeab
chimekinglsey <onlinekingsley@gmail.com>
2023-10-18 22:19:11 +0100

    simulate Cache

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,4 @@
+Another lightening fast database is here...Redis (REmote DIctionary Server)!
+It's both an in-memory cache (RAM) and supports almost every data type. It's a full blown database of it's own and has modules that support all kinds of data store--structured, unstructed, etc with an almost unbeatable read and write speed (upto 110,000 set per second)
+
+In this project, we will look at redis and it's python support (redis-py)
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+0. Writing strings to Redis
+"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """simulate a redis"""
+    def __init__(self, r: redis.Redis = redis.Redis()) -> None:
+        self._redis = r
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """generate a random uuid, store it in Redis, return the key"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+    
